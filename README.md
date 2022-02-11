1c7f70a78d6ac2c3366f62afd12212c2c51c3f1e
pollenjp <polleninjp@gmail.com>
2022-02-11 17:50:00 +0900

    Init app

---
+++ README.md
@@ -0,0 +1,6 @@
+# 2021-slack-bot
+
+```.env
+SLACK_BOT_USER_OAUTH_TOKEN="xoxb-****"
+SLACK_BOT_APP_LEVEL_TOKEN="xapp-****"
+```
---
+++ main.py
@@ -0,0 +1,31 @@
+# Standard Library
+import logging
+import os
+from logging import getLogger
+from pathlib import Path
+
+# Third Party Library
+from dotenv import load_dotenv
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+logging.basicConfig(
+    format="[%(asctime)s][%(levelname)s][%(filename)s:%(lineno)d] - %(message)s",
+    level=logging.WARNING,
+)
+logger = getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+load_dotenv(dotenv_path=Path(__file__).parents[0] / ".env")
+
+CONST_SLACK_BOT_USER_OAUTH_TOKEN: str = os.environ.get("SLACK_BOT_USER_OAUTH_TOKEN", "")
+if not CONST_SLACK_BOT_USER_OAUTH_TOKEN:
+    raise RuntimeError("SLACK_BOT_USER_OAUTH_TOKEN is not set")
+CONST_SLACK_BOT_APP_LEVEL_TOKEN: str = os.environ.get("SLACK_BOT_APP_LEVEL_TOKEN", "")
+if not CONST_SLACK_BOT_APP_LEVEL_TOKEN:
+    raise RuntimeError("SLACK_BOT_APP_LEVEL_TOKEN is not set")
+
+app = App(token=CONST_SLACK_BOT_USER_OAUTH_TOKEN)
+
+if __name__ == "__main__":
+    SocketModeHandler(app, CONST_SLACK_BOT_APP_LEVEL_TOKEN).start()
